44ee540cda1366bf52a35294bfec338e0a0c13ab
DanielOv1103 <danieloliveirav1103@gmail.com>
2024-05-16 09:42:02 -0500

    Update 5

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,11 @@ import './index.css'
 import {createBrowserRouter, RouterProvider} from "react-router-dom"
 
 import Root, { loader as rootLoader, action as rootAction, } from './routes/root'
-import ErrorPage from './error-page'
-import Contact, {loader as contactLoader} from './routes/contact'
-import EditContact, {action as editAction,} from "./routes/edit"
-import {action as destroyAction} from "./routes/destroy"
+import ErrorPage from './error-page.jsx'
+import Contact, {loader as contactLoader, action as contactAction,} from './routes/contact.jsx'
+import EditContact, {action as editAction,} from "./routes/edit.jsx"
+import {action as destroyAction} from "./routes/destroy.jsx"
+import Index from './routes/index.jsx'
 
 
 
@@ -22,19 +23,31 @@ const router = createBrowserRouter([
     action: rootAction,
     children: [
       {
-        path: "contacts/:contactId",
-        element: <Contact />,
-        loader: contactLoader,
-      },
-      {
-        path: "contacts/:contactId/edit",
-        element: <EditContact />,
-        loader: contactLoader,
-        action: editAction,
-      },
-      {
-        path: "contacts/:contactId/destroy",
-        action: destroyAction,
+        errorElement: <ErrorPage/>,
+        children: [
+          {
+            index: true,
+            element: <Index/>
+          },
+
+          {
+            path: "contacts/:contactId",
+            element: <Contact />,
+            loader: contactLoader,
+            action: contactAction,
+          },
+          {
+            path: "contacts/:contactId/edit",
+            element: <EditContact />,
+            loader: contactLoader,
+            action: editAction,
+          },
+          {
+            path: "contacts/:contactId/destroy",
+            action: destroyAction,
+            errorElement: <div>Oops! There was an error.</div>,
+          },
+        ]
       },
     ],
   },
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,5 +1,6 @@
-import { Outlet, NavLink, useLoaderData,  Form, redirect, useNavigation, } from "react-router-dom";
+import { Outlet, NavLink, useLoaderData,  Form, redirect, useNavigation, useSubmit } from "react-router-dom";
 import { getContacts, createContact } from "../contacts.js";
+import {useEffect} from "react";
 
 
 export async function action() {
@@ -7,30 +8,56 @@ export async function action() {
     return redirect(`/contacts/${contact.id}/edit`);
 }
 
+export async function loader({ request }) {
+    const url = new URL(request.url);
+    const q = url.searchParams.get("q");
+    const contacts = await getContacts(q);
+    return { contacts, q };
+}
+
 
 
 export default function Root (){
 
-    const { contacts } = useLoaderData();
+    const { contacts, q } = useLoaderData();
     const navigation = useNavigation();
+    const submit = useSubmit();
+
+    const searching =
+    navigation.location &&
+    new URLSearchParams(navigation.location.search).has(
+        "q"
+    );
+
+    useEffect(() => {
+        document.getElementById("q").value=q;
+    }), [q]
 
     return (
         <>
             <div id="sidebar">
                 <h1>React Routes Contacts</h1>
                 <div >
-                    <form id="search-form" role="search">
+                    <Form id="search-form" role="search">
                         <input
                             id="q"
+                            className={searching ? "loading" : ""}
                             aria-label="Search contacts"
                             placeholder="Search"
                             type="search"
                             name="q"
+                            defaultValue={q}
+                            onChange={(event) => {
+                                const isFirstSearch = q == null;
+                                submit(event.currentTarget.form, {
+                                replace: !isFirstSearch,
+                                });
+                            }}
                         />
                         <div
                             id="search-spinner"
                             aria-hidden
-                            hidden={true}
+                            hidden={!searching}
                         >
                         </div>
 
@@ -38,7 +65,7 @@ export default function Root (){
                             className="sr-only"
                             aria-live="polite"
                         ></div>
-                    </form>
+                    </Form>
                     <Form method="post">
                         <button type="submit">New</button>
                     </Form>
@@ -77,9 +104,4 @@ export default function Root (){
             </div>
         </>
     )
-}
-
-export async function loader() {
-    const contacts = await getContacts();
-    return { contacts };
 }
